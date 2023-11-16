074e44844b509f734795c1f897be15e69419ab2b
joaoreis28 <reisjoaomendes@gmail.com>
2023-11-16 15:52:36 -0300

    Exercises.

---
+++ Introductory_Problems/BitStrings.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define MOD 1000000007
+
+ll expbin( ll a, ll exp)
+{
+    if(exp == 0)
+        return 1;
+    if(exp & 1)
+        return ((a * expbin(a, exp -1)) % MOD);
+    ll b = expbin(a, exp/2);
+    return (b*b) % MOD;
+}
+
+
+
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    ll n ;
+    cin >> n;
+    cout << expbin(2, n);
+    return 0;
+}
