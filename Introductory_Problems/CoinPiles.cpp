4b3b8464b6e24df40b8ba268a982d95f7e2787af
Hippo Joao <reisjoaomendes@gmail.com>
2024-05-23 11:04:30 -0300

    Exercise 11.

---
+++ Introductory_Problems/CoinPiles.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+
+        
+    // 1 *x + 2*y = a
+    // 2*x + 1*y = b
+
+
+    // 9 9 
+
+
+    }
+
+
+    return 0;
+}
