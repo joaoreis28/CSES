df7724a5abd6c968e3421242aa25a4f89ff9a199
Hippo Joao <reisjoaomendes@gmail.com>
2024-07-15 21:30:07 -0300

    Matrix Expo

---
+++ Mathematics/ThrowingDice.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define MAX 1e9 + 7
+
+
+
+int main()
+{
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    ll n;
+
+    cin >> n;
+
+
+    /*A ^ n-1 * P   Matrix Exponentiation */
+
+
+
+    return 0;
+}
